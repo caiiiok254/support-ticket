cc29138e1824354af5a7c9c6410be9edb16c38f3
caiiiok254 <kiskamiska2000@gmail.com>
2020-03-28 14:56:52 +0700

    AppMailer configured, now new tickets mailing to the ticket creator. Controlls and blade for single ticket show added.

--- app/Http/Controllers/TicketsController.php
+++ app/Http/Controllers/TicketsController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
+use App\Ticket;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Mailers\AppMailer;
 
 class TicketsController extends Controller
 {
@@ -18,7 +22,9 @@ class TicketsController extends Controller
      */
     public function index()
     {
-        //
+        $tickets = Ticket::paginate(10);
+
+        return view('tickets.index', compact('tickets'));
     }
 
     /**
@@ -28,7 +34,9 @@ class TicketsController extends Controller
      */
     public function create()
     {
-        //
+        $categories = Category::all();
+
+        return view('tickets.create', compact('categories'));
     }
 
     /**
@@ -37,20 +45,50 @@ class TicketsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, AppMailer $mailer)
     {
-        //
+        $this->validate($request, [
+            'title' => 'required',
+            'category' => 'required',
+            'priority' => 'required',
+            'message' => 'required'
+        ]);
+
+        $ticket = new Ticket([
+            'title' => $request->input('title'),
+            'user_id' => Auth::user()->id,
+            'ticket_id' => strtoupper(str_random(10)),
+            'category_id' => $request->input('category'),
+            'priority' => $request->input('priority'),
+            'message' => $request->input('message'),
+            'status' => "Open"
+        ]);
+
+        $ticket->save();
+
+        $mailer->sendTicketInformation(Auth::user(), $ticket);
+
+        return redirect()->back()->with("status", "A ticket with ID: #$ticket->ticket_id has been opened.");
+    }
+
+    public function userTickets()
+    {
+        $tickets = Ticket::where('user_id', Auth::user()->id)->paginate(10);
+
+        return view('tickets.user_tickets', compact('tickets'));
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int  $ticket_id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show($ticket_id)
     {
-        //
+        $ticket = Ticket::where('ticket_id', $ticket_id)->firstOrFail();
+
+        return view('tickets.show', compact('ticket'));
     }
 
     /**
