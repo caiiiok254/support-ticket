e94c32fe4d14d816b9978518221b6eaee66fbd08
caiiiok254 <kiskamiska2000@gmail.com>
2020-03-28 15:13:13 +0700

    Add reply to tickets. Answers from manager mailing to the ticket owner

--- app/Mailers/AppMailer.php
+++ app/Mailers/AppMailer.php
@@ -39,7 +39,15 @@ class AppMailer
 
     public function sendTicketComments($ticketOwner, $user, Ticket $ticket, $comment)
     {
+        $this->to = $ticketOwner->email;
 
+        $this->subject = "RE: $ticket->title (Ticket ID: $ticket->ticket_id)";
+
+        $this->view = 'emails.ticket_comments';
+
+        $this->data = compact('ticketOwner', 'user', 'ticket', 'comment');
+
+        return $this->deliver();
     }
 
     public function sendTicketStatusNotification($ticketOwner, Ticket $ticket)
@@ -53,7 +61,6 @@ class AppMailer
 
             $message->from($this->fromAddress, $this->fromName)
                 ->to($this->to)->subject($this->subject);
-
         });
     }
 }
